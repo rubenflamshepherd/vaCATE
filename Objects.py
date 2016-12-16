c0caa54b89732b7a32a992661110723cf6546cd2
Ruben Labtop <ruben.flam.shepherd@mail.utoronto.ca>
2016-12-15 19:04:11 -0500

    Minor changes to doc strings of Operations.py. Major overhaul of doc strings in Objects.py

--- Objects.py
+++ Objects.py
@@ -2,20 +2,71 @@ import numpy
 import math
 import Operations
 
+
 class Experiment(object):
+    '''Experiment containing all data
+
+    === Attributes ===
+    @type directory: str
+        Folder where our data is/analysis will output
+    @type analyses: list[Analysis]
+        Individual CATE run objects (includes current analysis implemented)
+    '''
     def __init__(self, directory, analyses):
+        '''Experiment object with all relevant data
+
+        @type self: Experiment
+        @type directory: str
+            Folder where our data is/analysis will output
+        @type analyses: list[Analysis]
+            Individual CATE run objects (includes current analysis implemented)
+        @rtype: None
+        '''
         self.directory = directory
         self.analyses = analyses # List of Analysis objects
 
+
 class Analysis(object):
+    '''Analysis of a single CATE run/replicate
+
+    === Attributes ===
+    @type kind: None | 'obj' | 'subj'
+        The kind of analysis currently implemented. Default in None
+    @type obj_num_pts: int
+        Number of points that objective analysis is to be done with. 
+        Set to 8 for the default objective analysis.
+    @type run: Run
+        Run object containing basic run data
+    @type xs_p1: ('', '') | (float, float)
+        x-values of boundaries of phase 1. Default are empty strings.
+    @type xs_p2: ('', '') | (float, float)
+        x-values of boundaries of phase 2. Default are empty strings.
+    @type xs_p3: ('', '') | (float, float)
+        x-values of boundaries of phase 3. Default are empty strings.                
+    '''
     def __init__(self, kind, obj_num_pts, run, xs_p1=('', ''), 
             xs_p2=('', ''), xs_p3=('', '')):
+        ''' Constructor of Analysis object
+
+        @type kind: None | 'obj' | 'subj'
+            The kind of analysis currently implemented. Default in None
+        @type obj_num_pts: int | None
+            Number of points that objective analysis is to be done with. 
+            Set to 8 for the default objective analysis.
+        @type run: Run
+            Run object containing basic run data
+        @type xs_p1: ('', '') | (float, float)
+            x-values of boundaries of phase 1. Default are empty strings.
+        @type xs_p2: ('', '') | (float, float)
+            x-values of boundaries of phase 2. Default are empty strings.
+        @type xs_p3: ('', '') | (float, float)
+            x-values of boundaries of phase 3. Default are empty strings. 
+        @rtype: None
+        '''
         self.kind = kind # None, 'obj', or 'subj'
         self.obj_num_pts = obj_num_pts # None if not obj regression
         self.run = run
-        self.xs_p3 = xs_p3
-        self.xs_p2 = xs_p2
-        self.xs_p1 = xs_p1
+        self.xs_p3, self.xs_p2, self.xs_p1  = xs_p3, xs_p2, xs_p1
 
         # Default values are None unless assigned
         blank_phase = Phase(
@@ -37,11 +88,18 @@ class Analysis(object):
         self.influx, self.netflux, self.ratio = None, None, None
 
     def analyze(self):
+        '''Implement analysis based on settings from attributes.
+
+        Parameters are defined based on the phase limits that have been provided
+        The 'engine' of the analysis if you will.
+
+        @type self: Analysis
+        @rtype: None
         '''
-        Take Analysis object and define parameters based on which phase limits
-        have been provided.
-        Return Analysis object
-        '''
+        # Implement objective analysis. Note that objective analysis just uses a
+        #    set algorithm to set phase limits. After this if block the process
+        #    is the same of both objective and subjective analyses. A subjective
+        #    just allows the user to directly set the phase limits.
         if self.kind == 'obj':
             self.obj_x_start = self.run.x[-self.obj_num_pts:]
             self.obj_y_start = self.run.y[-self.obj_num_pts:]
@@ -53,6 +111,7 @@ class Analysis(object):
                 xs_p3=self.xs_p3, elut_ends_parsed=self.run.elut_ends_parsed,
                 elut_cpms_log=self.run.elut_cpms_log,
                 elut_ends=self.run.elut_ends)
+        # From here analysis is same for both objective and subjective analyses
         if self.xs_p3 != ('', ''):
             self.phase3 = Operations.extract_phase(
                 xs=self.xs_p3, 
@@ -62,7 +121,7 @@ class Analysis(object):
                 SA=self.run.SA, load_time=self.run.load_time)
             Operations.advanced_run_calcs(analysis=self)
             if self.xs_p2 != ('', '') and self.phase3.xs!= ('', ''):
-                # Set series' to be curvestripped
+                # Set series' to be curve-stripped
                 end_p12_index = Operations.x_to_index(
                     x_value=self.xs_p2[1], index_type='end',
                     x_series=self.run.elut_ends_parsed,
@@ -70,8 +129,8 @@ class Analysis(object):
                 self.x_p12 = self.run.x[: end_p12_index+1]
                 self.y_p12 = self.run.y[: end_p12_index+1]
                 # Curve strip phase 1 + 2 data of phase 3
-                # From here on data series potentially have 'holes' from ommitting
-                # negative log operations during curvestripping
+                # From here on data series potentially have 'holes' from
+                # ommitting negative log operations during curvestripping
                 self.x_p12_curvestrip_p3, self.y_p12_curvestrip_p3 = \
                     Operations.curvestrip(
                         x_series=self.x_p12, y_series=self.y_p12, 
@@ -94,21 +153,19 @@ class Analysis(object):
                         larger_x=self.run.elut_ends)
                     self.x_p1 = self.run.x[start_p1_index : end_p1_index+1]
                     self.y_p1 = self.run.y[start_p1_index : end_p1_index+1]
-                    # Set series' to be further curvestripped (already partially done)
+                    # Getting phase 1 data that has been already stripped of
+                    # phase 3 data
                     self.x_p1_curvestrip_p3 =\
                         self.x_p12_curvestrip_p3[start_p1_index : end_p1_index+1]
                     self.y_p1_curvestrip_p3 =\
                         self.y_p12_curvestrip_p3[start_p1_index : end_p1_index+1]
-                    # Curve strip phase 1 data of phase 3 (already stripped phase)
+                    # Curve-strip phase 2 data from phase 1
                     self.x_p1_curvestrip_p23, self.y_p1_curvestrip_p23 = \
                         Operations.curvestrip(
                             x_series=self.x_p1_curvestrip_p3,
                             y_series=self.y_p1_curvestrip_p3, 
                             slope=self.phase2.slope,
                             intercept=self.phase2.intercept)
-                    #print 'input', self.x_p1_curvestrip_p3
-                    #print 'ouput', self.x_p1_curvestrip_p23
-                    
                     self.phase1 = Operations.extract_phase(
                         xs=self.xs_p1, 
                         x_series=self.x_p1_curvestrip_p23,
@@ -116,15 +173,75 @@ class Analysis(object):
                         elut_ends=self.run.elut_ends,
                         SA=self.run.SA, load_time=self.run.load_time)
 
+
 class Run(object):
-    '''
-    Class that stores ALL data of a single CATE run.
-    This data includes values derived from objective or 
-    subjetive analyses (calculated within the class)
+    '''Basic data form a single CATE run/replicate
+
+    === Attributes ===
+    @type name: str
+        Name of run, used for identification purposes
+    @type SA: float
+        The specific activity of the loading solution used for the CATE
+            run/replicate; radiactivity(counts per min)/volume (mL).
+    @type rt_cnts: float
+        Radioactivity of the roots of the plant used (cpm).
+    @type sht_cnts: float
+        Radioactivity of the shoots of the plant used (cpm).
+    @type rt_wght: float
+        Weight of the roots of the plant used (grams).
+    @type gfact: float
+        Instrument-specific correction factor. Used to account of idiosyncrasies
+            of the detecting equiptment.
+    @type load_time: float
+        Amount of time plant used was placed in loading solution for (minutes).
+    @type elut_ends: list[float]
+        Time points that eluates were removed from plants (vs added to plants)
+    @type raw_cpms: list[float]
+        Eluate radioactivities as measured by detecting equitpment.
+    @type elut_cpms_gfact: list[float]
+        elut_cpms corrected for (multiplied by) g_fact
+    @type elut_cpms_gRFW: list[float]
+        elut_cpms_gfact corrected for (divided by) g_fact
+    @type elut_cpms_log: list[float]
+        Logarithmic conversion of elut_cpms_gRFW
+    @type elut_ends_parsed: list[float]
+        elut_ends with time points corresponding to eluates with no 
+            radioactivity removed.
     '''        
     def __init__(
     	   self, name, SA, rt_cnts, sht_cnts, rt_wght, gfact,
-    	   load_time, elut_ends, raw_cpms, elut_cpms):       
+    	   load_time, elut_ends, raw_cpms, elut_cpms):
+        '''Constructor of Run objects
+
+        @type self: Run
+        @type name: str
+            Name of run, used for identification purposes
+        @type SA: float
+            The specific activity of the loading solution used for the CATE
+                run/replicate; radiactivity(counts per min)/volume (mL).
+        @type rt_cnts: float
+            Radioactivity of the roots of the plant used (cpm).
+        @type sht_cnts: float
+            Radioactivity of the shoots of the plant used (cpm).
+        @type rt_wght: float
+            Weight of the roots of the plant used (grams).
+        @type gfact: float
+            Instrument-specific correction factor. Used to account of idiosyncrasies
+                of the detecting equiptment.
+        @type load_time: float
+            Amount of time plant used was placed in loading solution for (minutes).
+        @type elut_ends: list[float]
+            Time points that eluates were removed from plants (vs added to plants)
+        @type raw_cpms: list[float]
+            Eluate radioactivities as measured by detecting equitpment.
+        @type elut_cpms: list[float]
+            raw_cpms with blank values ('') replaced with 0s
+        @type x: ndarry
+            elut_ends_parsed converted to a numpy compatable array
+        @type y: ndarry
+            elut_cpms_log converted to a numpy compatable array
+        @rtype: None
+        '''       
         self.name = name # Text identifier extracted from col header in excel
         self.SA = SA
         self.rt_cnts = rt_cnts
@@ -144,23 +261,93 @@ class Run(object):
         self.x = numpy.array(self.elut_ends_parsed)
         self.y = numpy.array(self.elut_cpms_log)
 
+
 class Phase(object):
-    '''
-    Class that stores all data of a particular phase
+    ''' Data for a particular phase in our Analysis
+    
+    === Attributes ===
+    @type xs: (float, float) | ('', '')
+        x-values (from elut_ends_parsed) of boundaries of phase.
+    @type xy1: (float, float) | ('', '')
+        x, y coordinates for one end of regression line.
+        Used to plot phase in GUI.
+    @type xy2: (float, float) | ('', '')
+        x, y coordinates for other end of regression line.
+        Used to plot phase in GUI.
+    @type r2: float | ''
+        Coefficient of correlation (R^2) between <x_series> and <y_series>.
+    @type slope: float | ''
+        Slope of regression line between <x_series> and <y_series>.
+    @type intercept: float | ''
+        Intercept of regression line between <x_series> and <y_series>.
+    @type x_series: [float]
+        x_series of data. Generally elut_ends_parsed.
+    @type x_series: [float]
+        y_series of data. elut_cpms_log data is curve-stripped forms.
+    @type k: float | ''
+        Rate constant of the phase (slope *2.303).
+    @type t05: float | ''
+        Half-life of exchange of the phase (0.693/k).
+    @type r0: float | ''
+        Rate of radioisotope release from compartment at time = 0 (antilog of
+            intercept).
+    @type efflux: float | ''
+        Efflux from compartment (r0/SA).
+
+    === Representation Invariants ===
+    - Default values of attributes in blank phase are empty strings
     '''
     def __init__(
         self, xs, xy1, xy2, r2, slope, intercept, x_series, y_series,
         k, t05, r0, efflux):
+        ''' Constructor of Phase object.
+
+        @type self: Phase
+        @type xs: (float, float) | ('', '')
+            x-values (from elut_ends_parsed) of boundaries of phase.
+        @type xy1: (float, float) | ('', '')
+            x, y coordinates for one end of regression line.
+            Used to plot phase in GUI.
+        @type xy2: (float, float) | ('', '')
+            x, y coordinates for other end of regression line.
+            Used to plot phase in GUI.
+        @type r2: float | ''
+            Coefficient of correlation (R^2) between <x_series> and <y_series>.
+        @type slope: float | ''
+            Slope of regression line between <x_series> and <y_series>.
+        @type intercept: float | ''
+            Intercept of regression line between <x_series> and <y_series>.
+        @type x_series: [float]
+            x_series of data. Generally elut_ends_parsed.
+        @type x_series: [float]
+            y_series of data. elut_cpms_log data is curve-stripped forms.
+        @type k: float
+            Rate constant of the phase (slope *2.303).
+        @type t05: float
+            Half-life of exchange of the phase (0.693/k).
+        @type r0: float
+            Rate of radioisotope release from compartment at time = 0 (antilog of
+                intercept).
+        @type efflux: float
+            Efflux from compartment (r0/SA).
+        @rtype: None
+        '''
         self.xs = xs # paired tuple (x, y)
         self.xy1, self.xy2 = xy1, xy2 # Each is a paired tuple
-
         self.r2, self.slope, self.intercept = r2, slope, intercept
         self.x_series, self.y_series = x_series, y_series
         self.k, self.t05, self.r0, self.efflux = k, t05, r0, efflux
 
+
     def blank_phase(self):
+        '''Creating a blank phase
+
+        @type self: Phase
+        @rtype: Phase
+        '''
         return Phase(
             ('',''), ('',''), ('',''), '', '', '', [], [], '', '' ,'' ,'')
+
 
 if __name__ == "__main__":
     import Excel
--- Operations.py
+++ Operations.py
@@ -2,10 +2,11 @@ import math
 import numpy
 import Objects
 
+
 def grab_x_ys(elution_ends, slope, intercept):
     '''Given general line data, output ends of line for graphing.
 
-    @type elution_ends: [ints]
+    @type elution_ends: list[int]
         x-series of line
     @type slope: float
         slope of line
@@ -19,6 +20,7 @@ def grab_x_ys(elution_ends, slope, intercept):
     y1, y2 = intercept, slope * last_elution + intercept
     return (x1, y1), (x2, y2)
 
+
 def basic_run_calcs(rt_wght, gfact, elut_starts, elut_ends, elut_cpms):
     '''Given initial CATE data, return basic CATE data.
 
@@ -29,13 +31,13 @@ def basic_run_calcs(rt_wght, gfact, elut_starts, elut_ends, elut_cpms):
         root weight of plant
     @type gfact: float
         factor used to correct for measuring differences in detecting machines
-    @type elut_starts: [floats]
+    @type elut_starts: list[float]
         x-series representing times elutions STARTED
-    @type elut_ends: [floats]
+    @type elut_ends: list[float]
         x-series representing times elutions ENDED. No points removed yet.
-    @type elut_cpms: [floats]
+    @type elut_cpms: list[float]
         y-series representing times raw elution radiactivitiy in (in cpm)
-    @rtype: [floats], [floats], [floats], [floats]
+    @rtype: list[float], list[float], list[float], list[float]
         elution radioactivity corrected for G-factor, root weight, logged, and
         the elution end points parsed, respectively
     '''    
@@ -54,6 +56,7 @@ def basic_run_calcs(rt_wght, gfact, elut_starts, elut_ends, elut_cpms):
                 
     return elut_cpms_gfact, elut_cpms_gRFW, elut_cpms_log, elut_ends_parsed
 
+
 def get_obj_phase3(obj_num_pts, elut_ends_parsed, elut_cpms_log):
     '''Determine limits of phase 3 using objective regression.
 
@@ -67,11 +70,11 @@ def get_obj_phase3(obj_num_pts, elut_ends_parsed, elut_cpms_log):
 
     @type obj_num_pts: int
         number of points used to start objective regression
-    @type elut_ends_parsed:[floats]
+    @type elut_ends_parsed: list[float]
         elution end points with empty end points (paired with '' or 0) removed
-    @type elut_cpms_log: [floats]
+    @type elut_cpms_log: list[float]
         elution radioativity corrected for G-factor, root weight, and logged
-    @rtype: (float, float), [floats], [floats], [floats]
+    @rtype: (float, float), list[float], list[float], list[float]
         boundaries of phase 3, and r^2s, slopes, and intercepts (for testing)
     '''
     r2s = []
@@ -102,6 +105,7 @@ def get_obj_phase3(obj_num_pts, elut_ends_parsed, elut_cpms_log):
 
     return xs_p3, r2s, ms, bs  # r2s, ms, bs returned for testing
 
+
 def get_obj_phase12(xs_p3, elut_ends_parsed, elut_cpms_log, elut_ends):
     '''Determining boundaries of phase 1+2 using objective regression.
 
@@ -110,11 +114,11 @@ def get_obj_phase12(xs_p3, elut_ends_parsed, elut_cpms_log, elut_ends):
 
     @type xs_p3: (float, float)
         boundaries of phase 3
-    @type elut_ends_parsed:[floats]
+    @type elut_ends_parsed: list[float]
         elution end points with empty end points (paired with '' or 0) removed
-    @type elut_cpms_log: [floats]
+    @type elut_cpms_log: list[float]
         elution radioativity corrected for G-factor, root weight, and logged
-    @type elut_ends: [floats]
+    @type elut_ends: list[float]
         x-series representing times elutions ENDED. No points removed yet.
     @rtype: (float, float), (float, float), float
         best boundaries of phase 2 and 1, and their combined r2
@@ -143,6 +147,7 @@ def get_obj_phase12(xs_p3, elut_ends_parsed, elut_cpms_log, elut_ends):
         
     return xs_p2, xs_p1, highest_r2  # highest_r2 is returned for testing
 
+
 def extract_phase(xs, x_series, y_series, elut_ends, SA, load_time):
     '''Extract compartment analysis of phase parameters.
 
@@ -154,11 +159,11 @@ def extract_phase(xs, x_series, y_series, elut_ends, SA, load_time):
 
     @type xs: (float, float)
         boundaries of the phase that must be converted to indexs
-    @type x-series: [floats]
+    @type x-series: list[float]
         x-series from which we are extracting data
-    @type y-series: [floats]
+    @type y-series: list[float]
         y-series from which we are extracting data
-    @type elut_ends: [floats]
+    @type elut_ends: list[float]
         x-series representing times elutions ENDED. No points removed yet.
     @type SA: float
         specific activity of loading solution in cpm/ml
@@ -205,13 +210,14 @@ def extract_phase(xs, x_series, y_series, elut_ends, SA, load_time):
         phase_xs, xy1, xy2, r2, slope, intercept,
         x_phase, y_phase, k, t05, r0, efflux)
 
+
 def x_to_index(x_value, index_type, x_series, larger_x):
-    ''' Converts a data point in <x_series> to an index.
+    ''' Converts data point <x_value> in <x_series> to an index.
 
     Precondition: <x_value> in in <larger_x>
 
     This function is neccessary because we remove blank data points over the 
-        course of our analysis (e.g., elut_ends_parsed, etc.). Thus indexs
+        course of our analysis (e.g., elut_ends_parsed, etc.). Thus, indexs
         don't line up well across the data analysis and the x values of data
         points are instead used. This function converts these x values to 
         indexs that are relevant to the local data.
@@ -222,15 +228,14 @@ def x_to_index(x_value, index_type, x_series, larger_x):
         x_value to be converted to index
     @type index_type: 'start' | 'end'
         whether <x_value> is the start or end of a boundary
-    @type x_series: [floats]
+    @type x_series: list[float]
         data series in which <x_value> is being converted to an index
-    @type larger_x: [floats]
+    @type larger_x: list[float]
         data series which has no missing data
     @rtype: int
         index that <x_value> has been converted to
     '''
     while x_value not in x_series:
-        #print x_value, x_series
         new_index = x_to_index(x_value, index_type, larger_x, larger_x)
         if index_type == 'start':
             x_value = larger_x[new_index + 1]
@@ -244,7 +249,7 @@ def x_to_index(x_value, index_type, x_series, larger_x):
 def advanced_run_calcs(analysis):
     '''Later CATE calculations that require some already extracted parameters.
 
-    Entire analysis object is imported because many parameters needed and
+    Entire <analysis> object is imported because many parameters needed and
     allows attributes to be manipulated directly.
 
     @type analysis: Analysis
@@ -263,11 +268,11 @@ def advanced_run_calcs(analysis):
     analysis.poolsize = analysis.influx * analysis.phase3.t05 / (3 * 0.693)
 
 def linear_regression(x_series, y_series):
-    '''Linear regression of x_series and y_series
+    '''Linear regression of <x_series> and <y_series>
 
-    @type x_series: [floats]
+    @type x_series: list[float]
         <x_series> we are using for linear regression
-    @type y_series: [floats]
+    @type y_series: list[float]
         <y_series> we are using for linear regression
     @rtype: float, float, float
         r^2, m (slope), and b (intercept) of y=mx+b
